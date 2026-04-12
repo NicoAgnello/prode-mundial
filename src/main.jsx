d534beae7bf092e4fd4fadd76d3dd2b39d3836f1
NicoAgnello <nikoagnello1@gmail.com>
2026-04-12 06:11:51 -0300

    fix auth0 cache localstorage

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Auth0Provider } from '@auth0/auth0-react'
-import { BrowserRouter } from 'react-router-dom'
-import App from './App'
-import './styles/global.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./styles/global.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Auth0Provider
       domain={import.meta.env.VITE_AUTH0_DOMAIN}
@@ -13,10 +13,12 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       authorizationParams={{
         redirect_uri: window.location.origin,
       }}
+      useRefreshTokens={true}
+      cacheLocation="localstorage"
     >
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Auth0Provider>
-  </React.StrictMode>
-)
+  </React.StrictMode>,
+);

2435e4eb0cc71bd82d8fc7f2c7899d3e07f4d81d
NicoAgnello <nikoagnello1@gmail.com>
2026-04-12 15:31:00 -0300

    fix unwind preserveNullAndEmptyArrays

--- api/predicciones.js
+++ api/predicciones.js
@@ -27,7 +27,7 @@ export default async function handler(req, res) {
               as: 'partido',
             },
           },
-          { $unwind: { path: '$partido', preserveNullAndEmpty: true } },
+          { $unwind: { path: '$partido', preserveNullAndEmptyArrays: true } },
           { $sort: { 'partido.fecha': 1 } },
         ])
         .toArray()
